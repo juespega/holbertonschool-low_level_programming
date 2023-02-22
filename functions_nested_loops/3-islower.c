1554c547d41bc011565c96cea3790dfd0b60c815
juespega <juespega666@gmail.com>
2023-02-22 08:52:00 -0800

    Write a function that checks for lowercase character.

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ *int _islower - checks for lowercase character.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
